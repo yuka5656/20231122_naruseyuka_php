45c4e33756bf36df9876f359245323e258106a8b
naruse yuka <rikoricenantenred@gmail.com>
2023-11-24 23:27:08 +0900

    画像

---
+++ config/cities.php
@@ -0,0 +1,39 @@
+<?php
+
+$cities = [
+    [
+        'name' => 'シドニー',
+        'time_zone' => 'Australia/Sydney',
+        'img' => 'australia.jpg'
+    ],
+    [
+        'name' => '東京',
+        'time_zone' => 'Asia/Tokyo',
+        'img' => 'japan.jpg'
+    ],
+    [
+        'name' => '上海',
+        'time_zone' => 'Asia/Shanghai',
+        'img' => 'china.jpg'
+    ],
+    [
+        'name' => 'モスクワ',
+        'time_zone' => 'Europe/Moscow',
+        'img' => 'russia.jpg'
+    ],
+    [
+        'name' => 'ロンドン',
+        'time__zone' => 'Europe/London',
+        'img' => 'british.jpg'
+    ],
+    [
+        'name' => 'ヨハネスブルグ',
+        'time_zone' => 'Africa/Johannesburg',
+        'img' => 'south-africa.jpg'
+    ],
+    [
+        'name' => 'ニューヨーク',
+        'time_zone' => 'America/New_york',
+        'img' => 'america.jpg'
+    ]
+    ];
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/sanitize.css">
@@ -18,7 +19,9 @@
   </header>
   <main>
     <div class="search-form__content">
-      <h2>日本と世界の時間を比較</h2>
+      <div class="search-form__heading">
+        <h2>日本と世界の時間を比較</h2>
+      </div>
     </div>
     <form class="search-form" action="result.php" method="get">
       <div class="search-form__item">
